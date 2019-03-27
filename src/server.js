ccbb046b2c0add7505c6080178270de714520c0c
Stephen Park <stephen@callbox.com>
2019-03-27 18:42:37 -0500

    Parentheses.

--- src/server.js
+++ src/server.js
@@ -19,7 +19,9 @@ client.on('message', msg => {
 // Create an event listener for new guild members
 client.on("guildMemberAdd", (member) => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(ch => (ch.name) === "general");
+  const channel = member.guild.channels.find( (ch) => 
+    ch.name === "general"
+  );
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
