29b82f43bc1e99c1db593b06c4e1505434d5b43c
Stephen Park <stephen@callbox.com>
2019-04-06 13:07:16 -0500

    Fix runtime issues from int/float conversions.

--- src/classes/Droppables.js
+++ src/classes/Droppables.js
@@ -12,23 +12,23 @@ export default class Droppable {
      * Key value pair mapping a drop threshold key to a crate.
      */
     this.crateTable = {
-      0.40: { crate: "Starter" },
-      0.70: { crate: "Standard" },
-      0.80: { crate: "Pro" },
+      0.4: { crate: "Starter" },
+      0.7: { crate: "Standard" },
+      0.8: { crate: "Pro" },
       0.95: { crate: "Unleashed" },
       0.99: { crate: "Dableashed" },
     };
     this.buffTable = {
-      0.40: { crate: "Starter" },
-      0.70: { crate: "Standard" },
-      0.80: { crate: "Pro" },
+      0.4: { crate: "Starter" },
+      0.7: { crate: "Standard" },
+      0.8: { crate: "Pro" },
       0.95: { crate: "Unleashed" },
       0.99: { crate: "Dableashed" },
     };
     this.gearTable = {
-      0.40: { crate: "Starter" },
-      0.70: { crate: "Standard" },
-      0.80: { crate: "Pro" },
+      0.4: { crate: "Starter" },
+      0.7: { crate: "Standard" },
+      0.8: { crate: "Pro" },
       0.95: { crate: "Unleashed" },
       0.99: { crate: "Dableashed" },
     };
