78db1a6a845a3d2009cd7d8e93d821cf142cd3c4
Stephen Park <stephen@callbox.com>
2019-11-16 15:54:38 -0600

    Upgrade to the GIPHY API.

--- src/commands/api/gifsearch.js
+++ src/commands/api/gifsearch.js
@@ -2,28 +2,25 @@ import * as Winston from "winston";
 import "dotenv/config";
 import axios from "axios";
 
-const TENOR_KEY = process.env.TENOR_API_KEY;
+const GIPHY_KEY = process.env.GIPHY_API_KEY;
 
 const exec = async (msg, searchTerm) => {
   const encodedSearchTerm = encodeURIComponent(searchTerm);
-  const contentFilter = "low";
-  const mediaFilter = "basic";
-  const queryLimit = 1;
-  const tenorRandomSearchEndpoint = `https://api.tenor.com/v1/random?key=${TENOR_KEY}&q=${encodedSearchTerm}&contentfilter=${contentFilter}&media_filter=${mediaFilter}&limit=${queryLimit}`;
-  const tenorRandomSearch = await axios.get(tenorRandomSearchEndpoint);
+  const contentFilter = "pg-13";
+  const giphyRandomSearchEndpoint = `https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_KEY}&tag=${encodedSearchTerm}&rating=${contentFilter}`;
+  const giphyRandomSearch = await axios.get(giphyRandomSearchEndpoint);
 
-  if (tenorRandomSearch.data) {
-    const result = tenorRandomSearch.data.results[0];
+  if (giphyRandomSearch.data.meta.status === 200) {
+    const dabUrl = giphyRandomSearch.data.data.url;
 
-    if (result) {
-      const gifUrl = result.media[0].gif.url;
-      msg.channel.send(gifUrl);
+    if (dabUrl) {
+      msg.channel.send(dabUrl);
     } else {
       msg.channel.send(`No results found for search term: ${searchTerm}`);
     }
   } else {
-    Winston.error(tenorRandomSearch);
-    msg.channel.send("The Tenor gif API seems to be having issues right now! Please try again later.");
+    Winston.error(giphyRandomSearch);
+    msg.channel.send("The GIPHY gif API seems to be having issues right now! Please try again later.");
   }
 };
 
--- src/commands/api/pickRandomDab.js
+++ src/commands/api/pickRandomDab.js
@@ -3,24 +3,21 @@ import "dotenv/config";
 import axios from "axios";
 import * as dabIncrement from "../../utils/dabIncrement";
 
-const TENOR_KEY = process.env.TENOR_API_KEY;
+const GIPHY_KEY = process.env.GIPHY_API_KEY;
 
 const exec = async (msg) => {
   const searchTerm = "dab";
-  const contentFilter = "low";
-  const mediaFilter = "basic";
-  const queryLimit = 1;
-  const tenorRandomSearchEndpoint = `https://api.tenor.com/v1/random?key=${TENOR_KEY}&q=${searchTerm}&contentfilter=${contentFilter}&media_filter=${mediaFilter}&limit=${queryLimit}`;
-  const tenorRandomSearch = await axios.get(tenorRandomSearchEndpoint);
+  const contentFilter = "pg-13";
+  const giphyRandomSearchEndpoint = `https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_KEY}&tag=${searchTerm}&rating=${contentFilter}`;
+  const giphyRandomSearch = await axios.get(giphyRandomSearchEndpoint);
 
-  if (tenorRandomSearch.data.results && tenorRandomSearch.data.results.length) {
-    const result = tenorRandomSearch.data.results[0];
-    const dabUrl = result.media[0].gif.url;
-
+  if (giphyRandomSearch.data.meta.status === 200) {
+    const dabUrl = giphyRandomSearch.data.data.url;
+    
     msg.channel.send(dabUrl);
   } else {
-    Winston.error(tenorRandomSearch);
-    msg.channel.send("The Tenor gif API seems to be having issues right now! Please try again later.");
+    Winston.error(giphyRandomSearch);
+    msg.channel.send("The GIPHY gif API seems to be having issues right now! Please try again later.");
   }
 
   dabIncrement.exec(msg.author.username.toLowerCase(), msg);
